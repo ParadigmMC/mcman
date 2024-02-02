0995e3a1d06fb3b2813b6c471d5fdb33e6c696e6
null <60427892+null8626@users.noreply.github.com>
2024-02-02 08:41:20 +0700

    fix[curserinth]: forgot iter() here

--- src/sources/curserinth.rs
+++ src/sources/curserinth.rs
@@ -88,7 +88,8 @@ impl<'a> CurserinthAPI<'a> {
                 .iter()
                 .filter(|v| {
                     if let Some(loader) = self.get_modrinth_name() {
-                        v.loaders.any(|l| l.as_str() == loader)
+                        v.loaders
+iter().any(|l| l.as_str() == loader)
                     } else {
                         true
                     }
