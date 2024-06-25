62e816c0cded04b8ea6dc082506cc9d20db3d03f
TheAlan404 <gokdenizbektas@gmail.com>
2024-06-25 17:06:21 +0000

    struct JavaProcess

--- src/api/tools/java/mod.rs
+++ src/api/tools/java/mod.rs
@@ -4,12 +4,41 @@ pub type JavaVersion = u32;
 mod installation;
 mod find;
 mod check;
+use std::{path::Path, process::Stdio};
+
+use anyhow::{anyhow, Result};
 use futures::StreamExt;
 pub use installation::*;
 pub use check::*;
+use tokio::process::{Child, Command};
+
+use crate::api::utils::pathdiff::diff_paths;
 
 pub struct JavaProcess {
-    
+    child: Child,
+}
+
+impl JavaProcess {
+    pub fn new(
+        dir: &Path,
+        java: &Path,
+        args: &[&str],
+    ) -> Result<Self> {
+        let dir = diff_paths(dir, std::env::current_dir()?)
+            .ok_or(anyhow!("Couldn't diff paths"))?;
+
+        let child = Command::new(java)
+            .args(args)
+            .current_dir(dir)
+            .stderr(Stdio::inherit())
+            .stdout(Stdio::piped())
+            .stdin(Stdio::piped())
+            .spawn()?;
+        
+        Ok(Self {
+            child,
+        })
+    }
 }
 
 pub async fn get_java_installations() -> Vec<JavaInstallation> {
@@ -21,4 +50,11 @@ pub async fn get_java_installations() -> Vec<JavaInstallation> {
         })
         .collect()
         .await
+}
+
+pub async fn get_java_installation_for(ver: JavaVersion) -> Option<JavaInstallation> {
+    get_java_installations()
+        .await
+        .into_iter()
+        .find(|v| v.version == ver)
 }
